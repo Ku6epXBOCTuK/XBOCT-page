65ad61569edf9534b516dfc365a56773d87c3373
Ku6epXBOCTuK <Ku6epXBOCTuK@gmail.com>
2026-04-10 18:15:45 +0500

    chore: fix eslint ts errors in svelte files

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,20 @@
+import tseslint from "typescript-eslint";
 import eslintPluginSvelte from "eslint-plugin-svelte";
 
 export default [
+	...tseslint.configs.recommended,
 	...eslintPluginSvelte.configs["flat/recommended"],
 	{
 		rules: {
 			"svelte/block-lang": ["error", { script: ["ts"] }],
+		},
+	},
+	{
+		files: ["**/*.svelte"],
+		languageOptions: {
+			parserOptions: {
+				parser: tseslint.parser,
+			},
 		},
 	},
 ];
