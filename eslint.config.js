aaea3691e9f460105979490f12bd9f20a9e0636d
Ku6epXBOCTuK <Ku6epXBOCTuK@gmail.com>
2026-04-10 10:02:27 +0500

    chore: setup eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+import eslintPluginSvelte from 'eslint-plugin-svelte'
+
+export default [
+  ...eslintPluginSvelte.configs['flat/recommended'],
+  {
+    rules: {
+      'svelte/block-lang': ['error', { script: ['ts'] }]
+    }
+  }
+]
